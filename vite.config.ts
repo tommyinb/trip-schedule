4c42ee43e0a7371733620e38ba8a8f0ef212555e
Tommy <tommy@tom.my>
2024-09-29 18:09:09 +0800

    drop pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "/trip-schedule",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Trip Schedule",
-        short_name: "Trip Schedule",
-        theme_color: "white",
-        icons: [
-          {
-            src: "/trip-schedule/icon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
